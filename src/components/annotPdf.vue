c9ae31bee889e5315dd40eed673bd3cf1f16dc69
sinatayebati <sinatayebati@gmail.com>
2024-05-19 16:06:38 -0500

    #10 added various annotations

--- src/components/annotPdf.vue
+++ src/components/annotPdf.vue
@@ -1,7 +1,53 @@
 <template>
   <div class="button-container">
-    <v-btn @click="triggerHighlightAnnotations">Add Highlight Annotations</v-btn>
-    <v-btn @click="triggerDeleteAnnotations">Delete Annotations</v-btn>
+    <v-row class="button-row">
+      <v-col cols="6">
+        <v-btn @click="setAnnotationType('highlight', { r: 255, g: 255, b: 0 })" block color="yellow darken-1">Add Highlight</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn @click="triggerDeleteAnnotations" block color="red darken-1">Delete Annotations</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="button-row">
+      <v-col cols="6">
+        <v-btn @click="setAnnotationType('squiggly', { r: 255, g: 0, b: 0 })" block color="green darken-1">Add Squiggly</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn @click="triggerDeleteAnnotations" block color="red darken-1">Delete Annotations</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="button-row">
+      <v-col cols="6">
+        <v-btn @click="setAnnotationType('underline', { r: 0, g: 255, b: 0 })" block color="blue darken-1">Add Underline</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn @click="triggerDeleteAnnotations" block color="red darken-1">Delete Annotations</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="button-row">
+      <v-col cols="6">
+        <v-btn @click="setAnnotationType('strike', { r: 255, g: 0, b: 0 })" block color="purple-lighten-1">Add Strike</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn @click="triggerDeleteAnnotations" block color="red darken-1">Delete Annotations</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="button-row">
+      <v-col cols="6">
+        <v-btn @click="setAnnotationType('square', { r: 0, g: 255, b: 0 })" block color="pink-lighten-1">Add Square</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn @click="triggerDeleteAnnotations" block color="red darken-1">Delete Annotations</v-btn>
+      </v-col>
+    </v-row>
+    <v-row class="button-row">
+      <v-col cols="6">
+        <v-btn @click="setAnnotationType('oval', { r: 0, g: 0, b: 255 })" block color="amber-lighten-1">Add Oval</v-btn>
+      </v-col>
+      <v-col cols="6">
+        <v-btn @click="triggerDeleteAnnotations" block color="red darken-1">Delete Annotations</v-btn>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -13,6 +59,15 @@ import { AnnotationFactory } from 'annotpdf';
 
 const pdfStore = piniaStore();
 const { pdfUrl } = storeToRefs(pdfStore);
+
+const annotationType = ref('highlight');
+const annotationColor = ref({ r: 255, g: 255, b: 0 });
+
+const setAnnotationType = (type, color) => {
+  annotationType.value = type;
+  annotationColor.value = color;
+  triggerHighlightAnnotations();
+};
 
 const triggerHighlightAnnotations = async () => {
   const pdfViewerIframe = document.getElementById('pdfViewer');
@@ -35,6 +90,17 @@ const triggerDeleteAnnotations = async () => {
 };
 
 const highlightAnnotation = async (viewerApp, pdfFactory) => {
+  // First, delete all existing annotations
+  const existingAnnotations = await pdfFactory.getAnnotations();
+  const flattenedAnnotations = existingAnnotations.flat();
+  if (flattenedAnnotations.length > 0) {
+    const annotationIdsToDelete = flattenedAnnotations.map(annot => annot.id);
+    const deletePromises = annotationIdsToDelete.map(annotationId => pdfFactory.deleteAnnotation(annotationId));
+    await Promise.all(deletePromises);
+    console.log('Deleted existing annotations');
+  }
+
+  // Then add new annotations
   const annotations = [];
   const page = 0;
   const contents = 'Trace-based Just-in-Time Type Specialization for Dynamic';
@@ -50,13 +116,26 @@ const highlightAnnotation = async (viewerApp, pdfFactory) => {
     rect,
     contents,
     author,
-    color: { r: 255, g: 255, b: 0 },
+    color: annotationColor.value,
     opacity: 0.7,
   });
 
   annotations.forEach(annotation => {
-    pdfFactory.createHighlightAnnotation(annotation);
+    if (annotationType.value === 'highlight') {
+      pdfFactory.createHighlightAnnotation(annotation);
+    } else if (annotationType.value === 'squiggly') {
+      pdfFactory.createSquigglyAnnotation(annotation);
+    } else if (annotationType.value === 'underline') {
+      pdfFactory.createUnderlineAnnotation(annotation);
+    } else if (annotationType.value === 'strike') {
+      pdfFactory.createStrikeOutAnnotation(annotation);
+    } else if (annotationType.value === 'square') {
+      pdfFactory.createSquareAnnotation(annotation);
+    } else if (annotationType.value === 'oval') {
+      pdfFactory.createCircleAnnotation(annotation);
+    }
   });
+
   const updatedPdfData = pdfFactory.write();
   viewerApp.open({ data: updatedPdfData });
   console.log('Added new annotations');
@@ -87,13 +166,22 @@ const deleteAnnotations = async (viewerApp, pdfFactory) => {
 .button-container {
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  gap: 0px;
   margin-bottom: 10px;
   width: 100%;
   padding: 10px;
-  background-color: #f5f5f5;
+  background-color: #F5F5F5;
   border: 1px solid #ddd;
   box-sizing: border-box;
-  height: 120vh;
+  height: 100%;
+}
+
+.button-row {
+  margin-bottom: 5px;
+}
+
+.v-btn {
+  height: 50px;
+  font-size: 16px;
 }
 </style>
