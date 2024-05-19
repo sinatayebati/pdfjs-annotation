897cad9891bf329410c606f8eb4a52c910078459
sinatayebati <sinatayebati@gmail.com>
2024-05-18 23:38:56 -0500

    #7 add and delete annotation functions added

--- src/components/pdfViewer.vue
+++ src/components/pdfViewer.vue
@@ -1,4 +1,9 @@
 <template>
+  <div class="switch-button">
+    <v-btn @click="triggerHighlightAnnotations">Add Highlight Annotations</v-btn>
+    <v-btn @click="triggerDeleteAnnotations">Delete Annotations</v-btn>
+  </div>
+
   <div>
     <iframe ref="pdfViewerIframe" id="pdfViewer" :src="viewerUrl" style="width: 100%; height: 100vh;"></iframe>
   </div>
@@ -8,19 +13,37 @@
 import { ref, onMounted } from 'vue';
 import { piniaStore } from '../stores/piniaStore';
 import { storeToRefs } from 'pinia';
+import { AnnotationFactory } from 'annotpdf';
 
 const pdfViewerIframe = ref(null);
 const viewerUrl = ref('/pdfjs-annotation/pdfjs-4.2.67-dist/web/viewer.html');
 
 const pdfStore = piniaStore();
 const { pdfUrl } = storeToRefs(pdfStore);
-
 const pdfPageDimensions = ref([]);
 
 // Call the function to load the PDF when the component is mounted
 onMounted(() => {
   loadPdf();
 });
+
+const triggerHighlightAnnotations = async () => {
+  const viewerApp = pdfViewerIframe.value.contentWindow.PDFViewerApplication;
+  if (viewerApp && viewerApp.pdfDocument) {
+    const data = await viewerApp.pdfDocument.getData();
+    const pdfFactory = new AnnotationFactory(data);
+    await highlightAnnotation(viewerApp, pdfFactory);
+  }
+};
+
+const triggerDeleteAnnotations = async () => {
+  const viewerApp = pdfViewerIframe.value.contentWindow.PDFViewerApplication;
+  if (viewerApp && viewerApp.pdfDocument) {
+    const data = await viewerApp.pdfDocument.getData();
+    const pdfFactory = new AnnotationFactory(data);
+    await deleteAnnotations(viewerApp, pdfFactory);
+  }
+};
 
 // Function to load the PDF file as Uint8Array
 async function fetchPdfAsUint8Array(pdfPath) {
@@ -69,4 +92,60 @@ async function loadPdf() {
   }
 }
 
+const highlightAnnotation = async (viewerApp, pdfFactory) => {
+  const annotations = [];
+    const page = 0;
+    const contents = 'Trace-based Just-in-Time Type Specialization for Dynamic';
+    const author = 'Auto Generated';
+    const rect = [
+      530, // x1
+      792 - 94.99, // y1
+      80.56, // x2
+      792 - 77.06 // y2
+    ];
+    annotations.push({
+      page,
+      rect,
+      contents,
+      author,
+      color: {r: 255, g: 255, b: 0},
+      opacity: 0.7,
+    });
+
+  annotations.forEach(annotation => {
+    pdfFactory.createHighlightAnnotation(annotation);
+  });
+  const updatedPdfData = pdfFactory.write();
+  viewerApp.open({ data: updatedPdfData });
+  console.log('Added new annotations');
+};
+
+const deleteAnnotations = async (viewerApp, pdfFactory) => {
+  if (viewerApp && viewerApp.pdfDocument) {
+    const existingAnnotations = await pdfFactory.getAnnotations();
+    const flattenedAnnotations = existingAnnotations.flat();
+    if (flattenedAnnotations.length > 0) {
+      const annotationIdsToDelete = flattenedAnnotations
+        .map(annot => annot.id);
+      const deletePromises = annotationIdsToDelete.map(annotationId => pdfFactory.deleteAnnotation(annotationId));
+
+      // Delete all existing annotations
+      await Promise.all(deletePromises);
+      console.log('Deleted existing annotations');
+      // delete annotation of the page passed and refresh
+      const updatedPdfData = pdfFactory.write();
+      await viewerApp.open({ data: updatedPdfData })
+    }
+  } else {
+    console.error('PDF document is not loaded or viewerApp is not initialized correctly.');
+  }
+};
 </script>
+
+<style>
+.switch-button {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+}
+</style>
--- src/stores/piniaStore.js
+++ src/stores/piniaStore.js
@@ -4,10 +4,14 @@ import { defineStore } from 'pinia';
 export const piniaStore = defineStore('pdfStore', {
   state: () => ({
     pdfUrl: '/pdfjs-annotation/pdfjs-4.2.67-dist/web/compressed.tracemonkey-pldi-09.pdf',
+    isHightlightAnnotationActive: false,
   }),
   actions: {
     setPdfUrl(url) {
       this.pdfUrl = url;
+    },
+    setisHightlightAnnotationActive(value) {
+      this.isHightlightAnnotationActive = value
     },
   },
 });
